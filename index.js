e8358fa07bbe859035f26dd906db87ca333a9494
Igor <91732297+FaFoLu@users.noreply.github.com>
2022-10-04 16:46:57 +0300

    first commit
    1.0.0

---
+++ index.js
@@ -0,0 +1,13 @@
+const puppeteer = require("puppeteer")
+
+async function start() {
+
+    const browser = await puppeteer.launch()
+    const page  = await browser.newPage()
+    await page.goto('https://www.blockchain.com/explorer/assets/btc')
+    await page.screenshot( {path: 'example.png'} )
+
+    await browser.close()
+};
+
+start()
