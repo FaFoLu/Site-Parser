40910b9b6aed4911e5c14fc631de5cf1c4cebcc5
Igor <91732297+FaFoLu@users.noreply.github.com>
2023-07-24 09:17:21 +0300

    Update index.js
    https://developer.chrome.com/articles/new-headless/

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer")
 
 async function start() {
 
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({ headless: "new" });
     const page  = await browser.newPage()
     await page.goto('site link')
     await page.screenshot( {path: 'example.png'} )
