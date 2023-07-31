c17bd82d90935c7c426644f1fe6bcae1a60b0c0b
Igor <91732297+FaFoLu@users.noreply.github.com>
2023-07-31 17:39:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-const puppeteer = require("puppeteer")
+// Підключаємо бібліотеку puppeteer
+const puppeteer = require("puppeteer");
 
+// Оголошуємо асинхронну функцію start, яка буде виконувати наші дії
 async function start() {
+  // Запускаємо новий браузер у headless режимі
+  const browser = await puppeteer.launch({ headless: "new" });
 
-    const browser = await puppeteer.launch({ headless: "new" });
-    const page  = await browser.newPage()
-    await page.goto('site link')
-    await page.screenshot( {path: 'example.png'} )
+  // Відкриваємо нову сторінку
+  const page = await browser.newPage();
 
-    await browser.close()
-};
+  try {
+    // Переходимо на веб-сторінку з вказаним посиланням (замініть 'site link' на справжнє посилання)
+    await page.goto("site link");
 
-start()
+    // Зберігаємо скріншот сторінки в файл з ім'ям 'example.png'
+    await page.screenshot({ path: "example.png" });
+
+    // Виводимо повідомлення про успішне збереження скріншоту
+    console.log("Зображення збережено у example.png");
+  } catch (error) {
+    // Якщо сталася помилка, виводимо її у консоль
+    console.error("Помилка:", error);
+  } finally {
+    // Завершуємо роботу браузера
+    await browser.close();
+  }
+}
+
+// Викликаємо функцію start для початку роботи з браузером і отримання скріншоту
+start();
